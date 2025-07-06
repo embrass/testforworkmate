be697ef18ef97ef7b42939da2f1192400e0404f2
Artyom. C <embrassuwu@gmail.com>
2025-07-06 15:54:58 +0300

    update

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import pytest
 import csv
 from main import load_csv, apply_filter, apply_aggregate, apply_sort
 
+
 @pytest.fixture
 def sample_csv_data(tmp_path):
     data = """name,brand,price,rating
@@ -40,6 +41,7 @@ def test_filter_by_brand(sample_csv_data):
     assert len(filtered) == 4
     assert all(phone["brand"] == "apple" for phone in filtered)
 
+
 def test_filter_by_price_gt(sample_csv_data):
     data = load_csv(sample_csv_data)
     filtered = apply_filter(data, "price>500")
@@ -52,15 +54,18 @@ def test_filter_by_rating_lt(sample_csv_data):
     filtered = apply_filter(data, "rating<4.5")
     assert len(filtered) == 4
 
+
 def test_aggregate_avg_price(sample_csv_data):
     data = load_csv(sample_csv_data)
     avg_price = apply_aggregate(data, "price=avg")
     assert avg_price == pytest.approx(602)
 
+
 def test_aggregate_max_rating(sample_csv_data):
     data = load_csv(sample_csv_data)
     max_rating = apply_aggregate(data, "rating=max")
     assert max_rating == 4.9
+
 
 def test_aggregate_min_price_xiaomi(sample_csv_data):
     data = load_csv(sample_csv_data)
